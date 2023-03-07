f7701398bef06b4069df3fe01e24f22386b96f80
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-03-07 20:47:28 +0100

    feat: :sparkles: create Character class

---
+++ src/lib/character.js
@@ -0,0 +1,13 @@
+export class Character {
+	constructor(name, gender, height, mass, hairColor, skinColor, eyeColor, movies, pictureURL) {
+		this.name = name;
+		this.gender = gender;
+		this.height = height;
+		this.mass = mass;
+		this.hairColor = hairColor;
+		this.skinColor = skinColor;
+		this.eyeColor = eyeColor;
+		this.movies = movies;
+		this.pictureURL = pictureURL;
+	}
+}
