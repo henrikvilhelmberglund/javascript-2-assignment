991a2a34982fa9b0a977dfd5000f224c57f2e50e
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2023-03-07 20:51:47 +0100

    feat: :tada: add Character class and import it
    first issue in Linear complete yay!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,56 +1,14 @@
 <script>
 	import { base } from "$app/paths";
-	const contents = [
-		{
-			name: "SvelteKit",
-			link: "https://kit.svelte.dev",
-			desc: "with adapter-static for easier Github Pages deployment",
-		},
-		{
-			name: "UnoCSS",
-			link: "https://github.com/unocss/unocss",
-			desc: "using Svelte scoped mode",
-		},
-		{
-			name: "Prettier Plugin TailwindCSS",
-			link: "https://github.com/tailwindlabs/prettier-plugin-tailwindcss",
-			desc: "for auto sorting of classes",
-		},
-		{
-			name: "Commit-and-Tag-Version",
-			link: "https://github.com/absolute-version/commit-and-tag-version",
-			desc: "for CHANGELOG generation and Github tagging of versions",
-		},
-		{
-			name: "PrismJS",
-			link: "https://prismjs.com",
-			desc: "with Prism-Svelte for syntax highlighting of code blocks",
-		},
-	];
+	import { Character } from "$lib/character";
 </script>
 
-<main class="[&>*]:m-4">
-	<h1 class="text-4xl">Welcome to SvelteKit - UnoCSS template!</h1>
-	<p>
-		Visit <a class="underline-blue-600 underline hover:text-blue-600" href="https://kit.svelte.dev"
-			>kit.svelte.dev</a> to read the documentation for SvelteKit
-	</p>
-	<p>This template includes:</p>
-	<ul>
-		{#each contents as { name, link, desc }}
-			<li class="list-none">
-				<div class="flex flex-col items-start pb-4 font-serif">
-					<a class="underline-blue-600 text-2xl underline hover:text-blue-600" href={link}>
-						{name}
-					</a>
-					<span>{desc}</span>
-				</div>
-			</li>
-		{/each}
-	</ul>
+<main class="flex flex-col items-center justify-center [&>*]:m-1">
+	<h1 class="text-7xl">Star Wars</h1>
+	<h1 class="text-5xl">The comparisons strike again</h1>
 </main>
 
-<footer class="m-4 h-64">
+<footer class="absolute right-0 bottom-0 m-4">
 	<p>
 		by
 		<a
