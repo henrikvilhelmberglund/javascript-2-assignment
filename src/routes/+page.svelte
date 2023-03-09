0b45931ae834e66c3860e052985e58c4576b9124
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-03-09 17:07:42 +0100

    feat: :sparkles: add changeTextColor() function that changes the color and displays a text box

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,6 +11,7 @@
 
 	async function createCharacters() {
 		printCharacterData = false;
+		showCompareCharactersButton = true;
 		let result1 = await api.fetchCharacterData(character1Name);
 		let result2 = await api.fetchCharacterData(character2Name);
 
@@ -44,7 +45,7 @@
 			name1,
 			gender1,
 			height1,
-			mass1,
+			mass1.replaceAll(",", ""),
 			hairColor1,
 			skinColor1,
 			eyeColor1,
@@ -56,7 +57,8 @@
 			name2,
 			gender2,
 			height2,
-			mass2,
+			// replace commas in mass to nothing
+			mass2.replaceAll(",", ""),
 			hairColor2,
 			skinColor2,
 			eyeColor2,
@@ -66,6 +68,7 @@
 		console.log(character2);
 	}
 	let printCharacterData = false;
+	let showCompareCharactersButton = false;
 
 	function caps(string) {
 		if (string === "hairColor" || string === "eyeColor" || string === "skinColor") {
@@ -73,6 +76,69 @@
 			return firstHalf[0].toUpperCase() + firstHalf.slice(1) + " color";
 		} else {
 			return string[0].toUpperCase() + string.slice(1);
+		}
+	}
+
+	let comparisonString = "";
+
+	function changeTextColor() {
+		for (const [key, value] of Object.entries(character1)) {
+			if (typeof value === "number") {
+				let element1 = document.querySelector(`#${key}_0`);
+				let element2 = document.querySelector(`#${key}_1`);
+				if (value > character2[key]) {
+					element1.className = "green";
+					element2.className = "red";
+					comparisonString += `${character1.name}'s ${key} is greater than ${character2.name}'s.^`;
+				} else if (value < character2[key]) {
+					element2.className = "green";
+					element1.className = "red";
+					comparisonString += `${character2.name}'s ${key} is greater than ${character1.name}'s.^`;
+				} else {
+					element1.className = "green";
+					element2.className = "green";
+					comparisonString += `${character1.name} and ${character2.name} have the same ${key}.^`;
+				}
+			} else if (
+				key === "name" ||
+				key === "gender" ||
+				key === "hairColor" ||
+				key === "skinColor" ||
+				key === "eyeColor"
+			) {
+				// not sure if eyeColor omission was on purpose or not, my guess is not, so I added it here
+				// also added name because why not
+				let element1 = document.querySelector(`#${key}_0`);
+				let element2 = document.querySelector(`#${key}_1`);
+				if (value === character2[key]) {
+					element1.className = "green";
+					element2.className = "green";
+					if (key.includes("Color")) {
+						comparisonString += `${character1.name} and ${character2.name} have the same ${
+							key.split("Color")[0]
+						} color.^`;
+					} else {
+						comparisonString += `${character1.name} and ${character2.name} are both ${value}.^`;
+					}
+				}
+			} else if (key === "movies") {
+				let element1 = document.querySelector(`#${key}_0`);
+				let element2 = document.querySelector(`#${key}_1`);
+				if (value.length > character2[key].length) {
+					element1.className = "green";
+					element2.className = "red";
+					comparisonString += `^${character1.name} has starred in more movies than ${character2.name}.^`;
+				} else if (value.length < character2[key].length) {
+					element2.className = "green";
+					element1.className = "red";
+					comparisonString += `^${character2.name} has starred in more movies than ${character1.name}.^`;
+				} else {
+					element1.className = "green";
+					element2.className = "green";
+					comparisonString += `^${character1.name} and ${character2.name} have starred in the same number of movies.^`;
+				}
+			}
+			// console.log("key: " + key, "value:" + value);
 		}
 	}
 </script>
@@ -83,48 +149,71 @@
 
 	<div>
 		<label for="character1Name">Pick character 1</label>
-		<select bind:value={character1Name} name="character1Name" id="character1Name">
+		<select
+			bind:value={character1Name}
+			on:change={(showCompareCharactersButton = false)}
+			name="character1Name"
+			id="character1Name">
 			{#each allCharacters as character}
 				<option value={character}>{character}</option>
 			{/each}
 		</select>
 
 		<label for="character2Name">Pick character 2</label>
-		<select bind:value={character2Name} name="character2Name" id="character2Name">
+		<select
+			bind:value={character2Name}
+			on:change={(showCompareCharactersButton = false)}
+			name="character2Name"
+			id="character2Name">
 			{#each allCharacters as character}
 				<option value={character}>{character}</option>
 			{/each}
 		</select>
 	</div>
 
-	<button on:click={createCharacters} class="rounded-lg bg-blue-400 p-2">Get data</button>
+	<button
+		on:click={() => {
+			createCharacters();
+			showCompareCharactersButton = true;
+		}}
+		class="rounded-lg bg-blue-400 p-2">Get data</button>
 
 	<div class="flex flex-col">
 		{#if character1 && character2}
 			<div class="items-center">
-				<button class="w-32 rounded-lg bg-blue-400 p-2" on:click={() => (printCharacterData = true)}
-					>Compare characters</button>
+				{#if showCompareCharactersButton}
+					<button
+						class="w-32 rounded-lg bg-blue-400 p-2"
+						on:click={() => {
+							comparisonString = "";
+							printCharacterData = true;
+							// change text color after 1 second
+							setTimeout(() => {
+								changeTextColor();
+							}, 1000);
+						}}>Compare characters</button>
+				{/if}
 			</div>
 			<article class="flex flex-row justify-between ">
-				{#each [character1, character2] as character}
+				{#each [character1, character2] as character, i}
 					<div class="flex-col items-center">
 						<h2 class="text-2xl">
 							{character.name ?? "empty"}
 						</h2>
 						<img class="w-[50%]" src={character.pictureURL} alt="a" />
 						{#if printCharacterData}
-							<div class="border-1 my-2 w-[50%] rounded-lg border-solid border-pink-500 p-2">
+							<div class="my-2 w-[50%] rounded-lg border-2 border-solid border-pink-500 p-2">
 								{#each Object.entries(character) as [key, value]}
 									{#if key !== "movies" && key !== "pictureURL"}
 										{#if key === "height"}
-											<p>{caps(key)}: {value}cm</p>
+											<p id="{key}_{i}">{caps(key)}: {value}cm</p>
 										{:else if key === "mass"}
-											<p>{caps(key)}: {value}kg</p>
+											<p id="{key}_{i}">{caps(key)}: {value}kg</p>
 										{:else}
-											<p>{caps(key)}: {value}</p>
+											<p id="{key}_{i}">{caps(key)}: {value}</p>
 										{/if}
 									{:else if key === "movies"}
-										<p>Stars in {value.length} movies</p>
+										<p id="movies_{i}">Stars in {value.length} movies</p>
 									{/if}
 								{/each}
 							</div>
@@ -132,6 +221,17 @@
 					</div>
 				{/each}
 			</article>
+			{#if comparisonString && printCharacterData}
+				<aside class="my-4 flex items-center rounded-lg border-2 border-solid border-pink-500 p-2">
+					<ul>
+						{#each comparisonString.split("^") as li}
+							<li>
+								{li}
+							</li>
+						{/each}
+					</ul>
+				</aside>
+			{/if}
 		{/if}
 	</div>
 </main>
@@ -145,3 +245,15 @@
 			><img class="inline w-6" src="{base}/Henrik.png" alt="avatar" />henrikvilhelmberglund</a>
 	</p>
 </footer>
+
+<style>
+	:global(.green) {
+		transition: all 300ms;
+		color: green;
+		font-weight: 700;
+	}
+	:global(.red) {
+		transition: all 300ms;
+		color: red;
+	}
+</style>
