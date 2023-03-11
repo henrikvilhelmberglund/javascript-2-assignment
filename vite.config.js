27a1099dc203f9d7f92388fe47b882112f60bf86
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-03-11 17:17:05 +0100

    feat: :sparkles: add web fonts config and fonts

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,24 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import UnoCSS from "unocss/vite";
 import presetUno from "@unocss/preset-uno";
+import presetWebFonts from "@unocss/preset-web-fonts";
 import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [
 		UnoCSS({
-			presets: [presetUno()],
+			presets: [
+				presetUno(),
+				presetWebFonts({
+					provider: "none",
+					fonts: {
+						// custom ones
+						starjedi: "Star Jedi",
+						starjedihollow: "Star Jedi Hollow",
+						starjedirounded: "Star Jedi Rounded",
+					},
+				}),
+			],
 			mode: "svelte-scoped",
 		}),
 		sveltekit(),
